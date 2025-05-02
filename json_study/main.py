6efffd71e3d837e1962da772b00f8b42830c1290
DaniloDiasLTDA <danilof.dias3@hotmail.com>
2025-05-02 13:39:40 -0300

    add function search by name and name == main

--- json_study/main.py
+++ json_study/main.py
@@ -10,10 +10,32 @@ try:
 except FileNotFoundError:
     print('Arquivo não existe.')
 
+# Buscar todos os clientes
 @app.route('/usuarios', methods=['GET'])
 def search_clients():
     return jsonify(data)
 
+# Buscar apenas um cliente
+
+def search_name(user_name):
+    user = next((client for client in data if client['name'].lower() == user_name.lower()), None)
+    if user:
+        return jsonify(user)
+    return jsonify({'mensagem': f'Usuario {user_name} não encontrado'})
+
+# Atualizar o nome de um cliente
+
+
+
+# Adicionar um cliente ao banco de dados
+
+
+
+# Excluir um cliente da lista
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
 
